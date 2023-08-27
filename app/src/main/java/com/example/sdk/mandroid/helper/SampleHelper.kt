440979f5810a6d59a25a45ca61a60ced37cf9238
Manish Kumar <manish@Manishs-MacBook-Air.local>
2023-08-28 04:25:17 +0530

    initial commit by Manish

---
+++ app/src/main/java/com/example/sdk/mandroid/helper/SampleHelper.kt
@@ -0,0 +1,10 @@
+package com.example.sdk.mandroid.helper
+
+import android.widget.Toast
+
+class SampleHelper {
+    
+    fun getSalutation(strName: String?): String{
+        return "Hello $strName"
+    }
+}
